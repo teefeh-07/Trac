00ad80a8327a1940dc3bc9ec6b73ca633e87c87d
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-30 05:32:04 +0100

    feat: add app layout and dashboard - part 2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,6 @@
 import React, { useState } from 'react';
 import { ConnectWallet } from './components/ConnectWallet';
 
+export default function App() {
+    const [productId, setProductId] = useState('');
+
